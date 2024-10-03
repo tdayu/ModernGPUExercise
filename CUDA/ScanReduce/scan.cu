960e04c18a5b6838b516eb52f86a9684eea8e330
dtou <da.yu.tou@cern.ch>
2024-10-03 17:00:01 +0800

    Updated scan into a working state.

--- CUDA/ScanReduce/scan.cu
+++ CUDA/ScanReduce/scan.cu
@@ -21,6 +21,11 @@
 #define FULL_MASK 0xFFFFFFFF
 #define WARP_SIZE 32
 
+enum ScanType {
+    Exclusive,
+    Inclusive
+};
+
 __device__ unsigned int laneid()
 {
     unsigned int laneid;
@@ -29,11 +34,11 @@ __device__ unsigned int laneid()
 }
 
 template<typename T>
-__device__ T warp_inclusive_scan(const T x){
+__device__ T warp_inclusive_scan(const T x, const unsigned mask = FULL_MASK, const int width = 32){
     T scan(x);
 
-    for (int delta = 1; delta < WARP_SIZE; delta *= 2){
-        T shuffled = __shfl_up_sync(FULL_MASK, scan, delta);
+    for (int delta = 1; delta < width; delta *= 2){
+        T shuffled = __shfl_up_sync(mask, scan, delta);
         if (laneid() >= delta) scan += shuffled;
     }
 
@@ -61,7 +66,7 @@ __device__ void shared_to_registers(const T * shared, T * registers){
 }
 
 template<typename T, unsigned NT, unsigned VT>
-__device__ void shared_to_global(const unsigned total, const T * global, T * shared){
+__device__ void shared_to_global(const unsigned total, T * global, T * shared){
     constexpr unsigned NV = NT * VT;
     #pragma unroll
     for (int i = 0; i < VT; i++){
@@ -77,6 +82,7 @@ __device__ void registers_to_shared(T * shared, const T * registers){
     for (int i = 0; i < VT; i++){
         shared[VT * threadIdx.x + i] = registers[i];
     }
+    __syncthreads();
 }
 
 template<typename T, unsigned VT>
@@ -91,23 +97,62 @@ template<typename T, unsigned NT>
 __device__ void cta_upsweep(const T x, T * shared_values, T * spine){
     T warp_scanned = warp_inclusive_scan(x);
 
-    shared_values[threadIdx.x] = warp_scanned;
+    // Scan the warp sums
     __syncthreads();
-    for (int delta = WARP_SIZE; delta < NT; delta *= 2){
-        if (threadIdx.x >= delta) shared_values[threadIdx.x] += shared_values[threadIdx.x - delta];
+    if (laneid() == WARP_SIZE - 1) shared_values[threadIdx.x / 32] = warp_scanned;
+    __syncthreads();
+
+    constexpr unsigned warp_spine_size = ( NT + WARP_SIZE - 1 ) / WARP_SIZE;
+    static_assert( warp_spine_size <= 32 );
+
+    const bool predicate = threadIdx.x < warp_spine_size;
+    const unsigned mask = __ballot_sync(FULL_MASK, predicate);
+    if (predicate){
+        T spine_scanned = shared_values[threadIdx.x];
+        spine_scanned = warp_inclusive_scan(spine_scanned, mask, warp_spine_size);
+        shared_values[threadIdx.x + 1] = spine_scanned;
+        if (threadIdx.x == 0) shared_values[0] = 0;
+        if (spine != nullptr && threadIdx.x == (warp_spine_size - 1)) spine[blockIdx.x] = spine_scanned;
+    }
+    __syncthreads();
+
+    T upsweep = warp_scanned + shared_values[threadIdx.x / 32];
+    __syncthreads();
+
+    shared_values[threadIdx.x + 1] = upsweep;
+    __syncthreads();
+}
+
+template<typename T, unsigned NT>
+__device__ void cta_shared_upsweep(const T x, T * shared_values, T * spine){
+    T warp_scanned = warp_inclusive_scan(x);
+    shared_values[threadIdx.x] = warp_scanned;
+
+    T value;
+    for (int delta = WARP_SIZE; delta < NT; delta++){
+        if (threadIdx.x >= delta) value = shared_values[threadIdx.x - delta];
+        __syncthreads();
+        if (threadIdx.x >= delta) shared_values[threadIdx.x] += value;
         __syncthreads();
     }
 
     const bool last_thread = (threadIdx.x == NT - 1);
-    if (last_thread) spine[NT] = shared_values[threadIdx.x];
+    if (last_thread) spine[blockIdx.x] = shared_values[threadIdx.x];
 }
 
-template<typename T, unsigned VT>
+template<typename T, unsigned VT, ScanType type>
 __device__ void cta_downsweep(T * shared_values, T * values){
-    const T downsweep = (threadIdx.x > 0) ? shared_values[threadIdx.x - 1] : 0;
+    if constexpr (type == ScanType::Exclusive) {
+        #pragma unroll
+        for (int i = VT - 1; i > 0; i--){
+            values[i] = values[i - 1];
+        }
+        values[0] = 0;
+    }
+
     #pragma unroll
     for (int i = 0; i < VT; i++){
-        values[i] += downsweep;
+        values[i] += shared_values[threadIdx.x];
     }
     __syncthreads();
 }
@@ -115,64 +160,171 @@ __device__ void cta_downsweep(T * shared_values, T * values){
 // NT = number of threads
 // VT = values per thread
 // NV = number of values per block
-template <typename T, unsigned NT, unsigned VT>
+template <typename T, unsigned NT, unsigned VT, ScanType type>
 __global__ void block_scan(
     const unsigned total,
     const T * input,
     T* output,
-    T* spine)
+    T* spine = nullptr)
 {
     constexpr unsigned NV = NT * VT;
-    __shared__ T shared_values[NV]; // shared storage
+    constexpr unsigned shared_size = (VT > 1) ? NV : NT + 1;
+    __shared__ T shared_values[shared_size]; // shared storage
     T values[VT]; // registers storage
 
-    global_to_shared<T, NT, VT>(input, shared_values);
+    global_to_shared<T, NT, VT>(total, input, shared_values);
     shared_to_registers<T, VT>(shared_values, values);
     linear_scan<T, VT>(values);
     cta_upsweep<T, NT>(values[VT - 1], shared_values, spine);
-    cta_downsweep<T, VT>(shared_values, values);
+    // cta_shared_upsweep<T, NT>(values[VT - 1], shared_values, spine);
+    cta_downsweep<T, VT, type>(shared_values, values);
     registers_to_shared<T, VT>(shared_values, values);
-    shared_to_global<T, NT, VT>(output, shared_values);
+    shared_to_global<T, NT, VT>(total, output, shared_values);
 }
 
-int main() {
+// NT = number of threads
+// VT = values per thread
+// NV = number of values per block
+template <typename T, unsigned NT, unsigned VT>
+__global__ void block_downsweep(
+    const unsigned total,
+    const T * spine,
+    T * values)
+{
+    constexpr unsigned NV = NT * VT;
+
+    // skip the first block since it will be 0
+    #pragma unroll
+    for (int i = 0; i < VT; i++){
+        const unsigned value_index = ( blockIdx.x + 1 ) * NV + NT * i + threadIdx.x;
+        const unsigned spine_index = blockIdx.x;
+        const T spine_value = spine[spine_index];
+        if (value_index < total) values[value_index] += spine_value;
+    }
+}
+
+// NT = number of threads
+// VT = values per thread
+// NV = number of values per block
+template <typename T, unsigned NT, unsigned VT>
+void scan(
+    const std::vector<T> host_input,
+    const unsigned number_of_elements,
+    std::vector<T>& host_output)
+{
+    constexpr unsigned NV = NT * VT;
+    std::vector <unsigned> number_of_blocks;
+
+    unsigned launch_blocks = ( number_of_elements + NV - 1 ) / NV;
+    number_of_blocks.push_back(launch_blocks);
+    while (launch_blocks > 1) {
+        launch_blocks = ( launch_blocks + NV - 1 ) / NV;
+        number_of_blocks.push_back(launch_blocks);
+    }
+    const unsigned number_of_sweeps = number_of_blocks.size() - 1;
+
+    T * dev_output;
+    std::vector <T*> dev_spines(number_of_sweeps);
+
+    CUDA_CALL(cudaSetDevice(0));
+    CUDA_CALL(cudaMalloc(&dev_output, (number_of_elements + 1) * sizeof(T)));
+    for (size_t i = 0; i < number_of_sweeps; i++){
+        CUDA_CALL(cudaMalloc(&(dev_spines[i]), number_of_blocks[i] * sizeof(T)));
+    }
+
+    CUDA_CALL(cudaMemcpy(dev_output, host_input.data(), number_of_elements * sizeof(T), cudaMemcpyHostToDevice));
+
+    if (number_of_sweeps > 0) {
+        block_scan<T, NT, VT, ScanType::Exclusive><<<number_of_blocks[0], NT>>>(
+            number_of_elements,
+            dev_output,
+            dev_output,
+            dev_spines[0]
+        );
+        for (size_t i = 0; i < number_of_sweeps - 1; i++){
+            block_scan<T, NT, VT, ScanType::Inclusive><<<number_of_blocks[i+1], NT>>>(
+                number_of_blocks[i],
+                dev_spines[i],
+                dev_spines[i],
+                dev_spines[i+1]
+            );
+        }
+        block_scan<T, NT, VT, ScanType::Inclusive><<<1, NT>>>(
+            number_of_blocks[number_of_sweeps - 1],
+            dev_spines[number_of_sweeps - 1],
+            dev_spines[number_of_sweeps - 1],
+            dev_output + number_of_elements
+        );
+        for (size_t i = 0; i < number_of_sweeps - 1; i++){
+            const unsigned source_spine = number_of_sweeps - i - 1;
+            const unsigned target_spine = source_spine - 1;
+            const unsigned downsweep_blocks = number_of_blocks[source_spine] - 1;
+            const unsigned number_of_spines = number_of_blocks[target_spine];
+
+            block_downsweep<unsigned, NT, VT><<<downsweep_blocks, NT>>>(
+                number_of_spines,
+                dev_spines[number_of_sweeps - i - 1],
+                dev_spines[number_of_sweeps - i - 2]
+            );
+        }
+        // Final spine to output downsweep
+        block_downsweep<unsigned, NT, VT><<<number_of_blocks[0] - 1, NT>>>(
+            number_of_elements,
+            dev_spines[0],
+            dev_output
+        );
+    }
+    else {
+        block_scan<T, NT, VT, ScanType::Exclusive><<<1, NT>>>(
+            number_of_elements,
+            dev_output,
+            dev_output,
+            dev_output + number_of_elements
+        );
+    }
+
+    CUDA_CALL(cudaMemcpy(host_output.data(), dev_output, (number_of_elements + 1) * sizeof(T), cudaMemcpyDeviceToHost));
+}
+
+int main(int argc, char** argv) {
     std::mt19937 gen(1234);
-    std::uniform_int_distribution<> prefix_sum_distribution(0, 128);
+    std::uniform_int_distribution<> prefix_sum_distribution(0, 32);
+
+    constexpr unsigned NT = 512;
+    constexpr unsigned VT = 3;
 
     // constexpr std::array<unsigned, 4> NT_values = { 128, 256, 512, 1024 };
     // constexpr std::array<unsigned, 8> VT_values = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
-    constexpr size_t number_of_elements = 1024 * 1024 * 32;
+    const size_t number_of_elements = (argc > 1) ? std::stol(argv[1]) : 1024 * 1024 * 16;
     std::vector<unsigned> host_input(number_of_elements);
-    std::vector<unsigned> host_output(number_of_elements);
+    std::vector<unsigned> host_output(number_of_elements + 1);
+    std::vector<unsigned> host_cpu_output(number_of_elements);
 
     for (size_t i = 0; i < number_of_elements; i++){
         host_input[i] = prefix_sum_distribution(gen);
     }
 
-    constexpr unsigned NT = 512;
-    constexpr unsigned VT = 5;
-    constexpr unsigned NV = NT * VT;
+    scan<unsigned, NT, VT>(host_input, number_of_elements, host_output);
 
-    unsigned * dev_input;
-    unsigned * dev_output;
-    unsigned * dev_spine;
+    // for (size_t i = 0; i < number_of_elements; i += NT * VT){
+    //     auto begin = host_input.begin() + i;
+    //     auto end   = host_input.begin() + min(host_input.end() - host_input.begin(), i + NT * VT);
+    //     auto output_iter = host_cpu_output.begin() + i;
+    //     std::exclusive_scan(begin, end, output_iter, 0);
+    // }
+    std::exclusive_scan(host_input.begin(), host_input.end(), host_cpu_output.begin(), 0);
 
-    constexpr unsigned number_of_blocks = ( number_of_elements + NV - 1 ) / NV;
+    unsigned nprint = 0;
+    for (size_t i = 0; i < number_of_elements; i++){
+        if (host_output[i] != host_cpu_output[i]) {
+            printf(
+            "Wrong values! Index: %u, GPU: %u, CPU: %u, Previous GPU: %u, Previous CPU: %u\n",
+            i, host_output[i], host_cpu_output[i], host_output[i - 1], host_cpu_output[i - 1]);
+            nprint++;
+        }
+        if (nprint >= 512) break;
+    }
 
-    CUDA_CALL(cudaSetDevice(0));
-    CUDA_CALL(cudaMalloc(&dev_input, number_of_elements * sizeof(unsigned)));
-    CUDA_CALL(cudaMalloc(&dev_output, number_of_elements * sizeof(unsigned)));
-    CUDA_CALL(cudaMalloc(&dev_spine, number_of_blocks * sizeof(unsigned)));
-
-    CUDA_CALL(cudaMemcpy(dev_input, host_input.data(), number_of_elements * sizeof(unsigned), cudaMemcpyHostToDevice));
-    block_scan<unsigned, NT, VT><<<number_of_blocks, NT>>>(
-        number_of_elements,
-        dev_input,
-        dev_output,
-        dev_spine
-    );
-    CUDA_CALL(cudaMemcpy(host_output.data(), dev_output, number_of_elements * sizeof(unsigned), cudaMemcpyDeviceToHost));
-
-    return 0;
+    printf("Total: GPU: %u, CPU: %u\n", host_output[number_of_elements], std::reduce(host_input.begin(), host_input.end()));
 }
